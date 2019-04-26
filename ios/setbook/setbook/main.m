d2b6072acf70e5a0a3df2ff6d6bcd8db8453cb06
tyzctyzc2 <tyzctyzc@126.com>
2019-04-26 20:05:32 +0800

    fix setting
    fix setting

--- ios/setbook/setbook/main.m
+++ ios/setbook/setbook/main.m
@@ -9,13 +9,41 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+NSString *getIPFromSetting() {
+    NSString *ip = @"192.168.0.1";
+    NSString * settingsBundle =[[NSBundle mainBundle]pathForResource:@"Settings"   ofType:@"bundle"];
+    if (!settingsBundle) {
+        NSLog(@"Could not find Settings.bundle");
+    }
+    else {
+        NSDictionary * settings =[NSDictionary dictionaryWithContentsOfFile: [settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+        
+        NSDictionary * Preference =[settings objectForKey:@"PreferenceSpecifiers"];
+    
+        for (NSDictionary * prefSpecification in Preference) {
+            NSString * key =[prefSpecification objectForKey:@"DefaultValue"];
+            if (key) {
+                ip = key;
+                NSLog(@"getting setting ~~~~~~~~~~~ %@", key);
+                break;
+            }
+        }
+    }
+    return ip;
+}
+
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        NSDictionary *appDefaults = [NSDictionary dictionaryWithObject:@"192.168.0.100"
-                                                                forKey:@"serveraddress_preference"];
-        [defaults registerDefaults:appDefaults];
-        [defaults synchronize];
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        NSString *address = [userDefaults objectForKey:@"serveraddress_preference"];
+        
+        if (address == nil) {
+            NSString *ip = getIPFromSetting();
+            NSDictionary *appDefaults = [NSDictionary dictionaryWithObject:ip
+                                                                    forKey:@"serveraddress_preference"];
+            [userDefaults registerDefaults:appDefaults];
+            [userDefaults synchronize];
+        }
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
